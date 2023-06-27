e8329ab55e4b0fb6cbfb39da4c68922036438170
FelippeChemello <felippechemello@gmail.com>
2023-06-26 23:12:22 -0300

    Change endpoint

--- api/index.py
+++ api/index.py
@@ -4,7 +4,11 @@ from newsplease import NewsPlease
 app = Flask(__name__)
 
 @app.route('/')
-def scrape():
+def index():
+    return jsonify({'message': 'Use /scrape?url=URL_TO_ARTICLE to scrape an article'})
+
+@app.route('/scrape')
+def scrape(request):
     url = request.args.get('url')
 
     article = NewsPlease.from_url(url)
